39ffe6965fdeedbf783e158c7abcdb06f9aeb2c9
Bryce Collins <brycecollins0@gmail.com>
2020-11-22 07:29:17 -0800

    Added ability to add a monitor through the discord chat, provided descriptions and usage for more complicated commands

--- src/discord/message_response.js
+++ src/discord/message_response.js
@@ -48,10 +48,12 @@ var responses = [
     {
         "exact": false,
         "compare": "!twitter_test",
+        "description": "Basic twitter search functionality - provide a twitter handle and a keyword list to search their recent tweets for that word. Will link the first one found.",
+        "usage": "!twitter_test <twitter_handle> <keyword>...",
         "action": (message) => {
             var variables = message.content.split(' ')
             if (variables.length > 1) {
-                twitter.find_recent_matching_tweets({ twitter_handle: variables[1], phrases_array: [variables[2]], all_present: true })
+                twitter.find_recent_matching_tweets({ twitter_handle: variables[1], phrases_array: variables.splice(2, variables.length - 2), all_present: false })
                     .then((id_array) => {
                         if (id_array == undefined || id_array.length == 0) {
                             message.channel.send(`I'm sorry, I wasn't able to find any tweets like that`)
@@ -68,14 +70,38 @@ var responses = [
     {
         'exact': false,
         'compare': '!monitoring',
+        'description': 'List all of the accounts being monitored and for what. Optionally, provide a twitter handle to filter to just that user',
+        'usage': '!monitoring <twitter_handle?>',
         'action': (message) => {
             var variables = message.content.split(' ')
             var mongo = new Mongo_dao()
             mongo.get_monitor_list((results) => {
-                var return_message_base = variables.length > 1 ? `Monitors on @${variables[1]}` : `I am watching:\n`
+                if (results.length < 1) {
+                    message.channel.send(`There are currently no monitors on @${variables[1]}`);
+                    return;
+                }
+                var return_message_base = variables.length > 1 ? `Monitors on ` : `I am watching:\n`
                 var return_message = results.map(monitor => { return `@${monitor.twitter_handle} for: ${JSON.stringify(monitor.keywords)}` }).join('\n')
                 message.channel.send(return_message_base + return_message)
             }, variables[1]);
+        }
+    },
+    {
+        'exact': false,
+        'compare': '!add_monitor',
+        'description': 'Add an account to monitor and the keywords to look for',
+        'usage': '!add_monitor <twitter_handle> <keyword> <keyword>...',
+        'action': (message) => {
+            var variables = message.content.split(' ')
+            var mongo = new Mongo_dao()
+            mongo.insert_monitor({
+                twitter_handle: variables[1],
+                keywords: variables.splice(2, variables.length - 2),
+                all_present: false
+            })
+                .then((result) => { message.channel.send(`I successfully created your requested monitor!`) })
+                .catch((err) => { message.channel.send(`I had an issue creating this monitor - No changes saved.`) })
+
         }
     }
 ]
